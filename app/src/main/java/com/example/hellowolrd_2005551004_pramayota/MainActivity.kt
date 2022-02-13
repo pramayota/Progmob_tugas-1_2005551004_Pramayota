def6a65dea2a79a67efd6392bf3f06b4fb6ecb6c
pramayota <pramayotalampung@student.unud.ac.id>
2022-02-13 20:26:08 +0800

    hello_wolrd

---
+++ app/src/main/java/com/example/hellowolrd_2005551004_pramayota/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hellowolrd_2005551004_pramayota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
